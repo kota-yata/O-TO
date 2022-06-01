ac687bb786107e1794df0787db83e19be89559ff
yoshito924 <kimu.murd.cisum@gmail.com>
2022-06-01 16:34:07 +0900

    小変更

--- public/js/ReverseLookup.js
+++ public/js/ReverseLookup.js
@@ -173,7 +173,7 @@ function NoteOnOff(st, Flet, MIDI_note_number) {
     for (let i = 0; i < PitchClassOnOff.length; i++) {
         OnOff[PitchClassOnOff[i]] = 1;
     };
-    console.log(OnOff)
+
     //この音の組み合わせを含む主なスケールを書き込む
     if (FingerboardOnOff.length > 0) {
         //モーダル・インターチェンジの候補をスケールの構成音とともに表示する関数(指板からコード・ネームやスケール名を逆引きする用)
@@ -223,8 +223,6 @@ function NoteOnOff(st, Flet, MIDI_note_number) {
             };
         };
     };
-
-
 };
 
 //モーダル・インターチェンジの候補をスケールの構成音とともに表示する関数(指板からコード・ネームやスケール名を逆引きする用)
